5d27472b5be009073c74099da037b93b691b3e74
Michael Crismali <michael.crismali@gmail.com>
2014-05-23 11:52:52 -0500

    added lockArgs function

--- spec/settler_spec.js
+++ spec/settler_spec.js
@@ -1,5 +1,49 @@
-describe('settler', function() {
-  it('is', function() {
-    expect(true).to.equal(true);
+describe('Settler', function() {
+  var subject;
+  var noop;
+  var add;
+
+  beforeEach(function() {
+    noop = function() {};
+    add = function(x, y) {
+      return x + y;
+    };
+  });
+
+  it('is an object', function() {
+    expect(Settler).to.be.object;
+  });
+
+  describe('lockArgs', function() {
+    var lockArgs;
+    var lockedFunc;
+
+    beforeEach(function() {
+      lockArgs = Settler.lockArgs;
+      lockedFunc = lockArgs(3, noop);
+    });
+
+    it('returns a function that throws an error if the number of arguments is incorrect', function() {
+      expect(function() {
+        lockedFunc('foo', 'bar');
+      }).to.throw(/Expected 3 arguments, but was given 2/);
+    });
+
+    it('executes the function when given the correct number of arguments', function() {
+      expect(function() {
+        lockedFunc('foo', 'bar', 'baz');
+      }).to.not.throw();
+    });
+
+    it('executes the function in the appropriate context', function() {
+      subject = {
+        worked: false,
+        work: lockArgs(0, function() {
+          this.worked = true;
+        })
+      };
+      subject.work();
+      expect(subject.worked).to.equal(true);
+    });
   });
 });
